549ffeb5b2e4533d0b70f881f4e5914d7c4d5463
hafid998 <sidiq0304@students.amikom.ac.id>
2023-06-04 22:33:31 +0700

    minggu ke8

---
+++ inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            person person1 = new person ("Kesmo", 20);
+            person1.Infoperson();
+
+            //INHERITANCE
+            Karyawan karyawan = new Karyawan("Jane Smith", "IT");
+            karyawan.InfoKaryawan();
+
+            Console.ReadLine();
+        }
+    }
+}
